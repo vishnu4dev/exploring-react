6b0fe85acda60a1d54b29f5a1ca751266076df19
vishnu4dev <2vishnukkumar@gmail.com>
2023-01-12 01:23:39 +0530

    feat: session02

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("h1",null, "Namaste React Day 01");
 const heading0 = React.createElement("h1",{id:"heading1"}, "Heading 1");
 const heading1 = React.createElement("h1",{id:"heading2"}, "Heading 2");
